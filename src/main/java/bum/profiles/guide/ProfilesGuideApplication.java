2c94865b32ceb4178dc1f64e73ef92935a8c7bc3
osparking <jbpark03@gmail.com>
2023-07-22 07:56:20 +0900

    initial - 3 dependencies

---
+++ src/main/java/bum/profiles/guide/ProfilesGuideApplication.java
@@ -0,0 +1,13 @@
+package bum.profiles.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesGuideApplication.class, args);
+	}
+
+}
